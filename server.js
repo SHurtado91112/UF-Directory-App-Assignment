4cfa00b1ce46d2001a497a3e3db31824a786cf93
SH-Arrow <shurt91112@gmail.com>
2017-08-23 23:39:31 -0400

    SH: First set up of server; 404 response and 200 response /listings data set

--- server.js
+++ server.js
@@ -16,6 +16,21 @@ var requestHandler = function(request, response) {
     HINT: explore the request object and its properties 
     http://stackoverflow.com/questions/17251553/nodejs-request-object-documentation
    */
+  var path = parsedUrl.pathname;
+  if(path == '/listings') {
+    const body = listingData;
+    console.log('Here');
+    response.writeHead(200, 'success', {'Content-Type' : 'text/plain'});
+    response.write(body);
+    response.end();
+  }
+  else {
+    console.log('Bad Access');
+    const body = 'Bad Gateway Error';
+    response.writeHead(404, 'Page Not Found', {'Content-Type' : 'error'});
+    response.write(body);
+    response.end(); 
+  }
 };
 
 fs.readFile('listings.json', 'utf8', function(err, data) {
@@ -23,4 +38,20 @@ fs.readFile('listings.json', 'utf8', function(err, data) {
     This callback function should save the data in the listingData variable, 
     then start the server. 
    */
+  
+  if(err) { 
+    throw err;
+  }
+
+  listingData = data;
+  // the server is now started, listening for requests on port 8080
+  server.listen(port, function() {
+    //once the server is listening, this callback function is executed
+    console.log('Server listening on: localhost:' + port);
+  });
 });
+
+// a server is created, but not started
+var server = http.createServer(requestHandler);
+
+console.log("Loading JSON data...");
