144cbfa7d0ac17921d6679c52122654b7c093b3c
SH-Arrow <shurt91112@gmail.com>
2017-09-01 20:54:29 -0400

    SH: finished basic functionality of application

--- listingController.js
+++ listingController.js
@@ -1,14 +1,43 @@
 angular.module('listings').controller('ListingsController', ['$scope', 'Listings', 
   function($scope, Listings) {
     $scope.listings = Listings;
-    $scope.detailedInfo = undefined;
+    $scope.detailedInfo = {
+            "code": undefined, 
+            "name": undefined, 
+            "coordinates": {
+                "latitude": undefined, 
+                "longitude": undefined
+            }, 
+            "address": undefined
+        };
 
     /* 
       Implement these functions in the controller to make your application function 
       as described in the assignment spec. 
      */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
+      $scope.clear = function() {
+          $scope.detailedInfo = {
+            "code": undefined, 
+            "name": undefined, 
+            "coordinates": {
+                "latitude": undefined, 
+                "longitude": undefined
+            }, 
+            "address": undefined
+        };
+      };
+      
+    $scope.addListing = function() {
+        $scope.listings.push($scope.detailedInfo);
+        $scope.clear();
+        
+        $('#modal-add').modal('toggle');
+        
+        $('.tableWrapper').scrollTop($('tfoot tr:last').position().top);
+    };
+    $scope.deleteListing = function(index) {
+        $scope.listings.splice(index, 1);
+    };
     $scope.showDetails = function(index) {
         console.log(index + " " + $scope.listings[index]);
         $scope.detailedInfo = $scope.listings[index];
