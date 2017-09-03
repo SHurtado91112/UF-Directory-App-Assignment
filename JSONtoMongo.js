c4f8c4b55770de8f96f5295b8d9e8881dc54dee0
SH-Arrow <shurt91112@gmail.com>
2017-09-02 22:44:03 -0400

    SH: assignment 3 base done

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -9,13 +9,33 @@ var fs = require('fs'),
     Listing = require('./ListingSchema.js'), 
     config = require('./config');
 
+var listingData = undefined;
+
 /* Connect to your database */
+mongoose.connect(config.db.uri);
 
 /* 
   Instantiate a mongoose model for each listing object in the JSON file, 
   and then save it to your Mongo database 
  */
 
+fs.readFile('listings.json', 'utf8', function(err, data) {  
+    if(err) { 
+        throw err;
+    }
+
+    listingData = JSON.parse(data).entries;
+    listingData.forEach(function(entry) {
+        var newlisting = new Listing(entry);
+
+        // save the user
+        newlisting.save(function(err) {
+            if (err) throw err;
+
+            console.log('Listing ' + entry.toString + ' created!');
+        });
+    });
+});
 
 /* 
   Once you've written + run the script, check out your MongoLab database to ensure that 
